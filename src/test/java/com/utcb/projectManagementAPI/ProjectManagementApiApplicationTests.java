960a4ef628dc543635a5232eb0f2c04b975bec9c
Mihai Croitoru <mihairaducroitoru@gmail.com>
2024-02-10 15:47:21 +0200

    initial commit

---
+++ src/test/java/com/utcb/projectManagementAPI/ProjectManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utcb.projectManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
